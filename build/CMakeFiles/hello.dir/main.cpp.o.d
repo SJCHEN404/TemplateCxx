bd82c99ae482f0405353dadd9f0d12861c43a30d
SJCHEN404 <chenshengjiao1912@163.com>
2022-08-23 10:54:53 +0800

    add TemplateCxx

---
+++ build/CMakeFiles/hello.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/hello.dir/main.cpp.o: \
+  /Users/SJCHEN/project/TemplateCxx/main.cpp \
+  /Users/SJCHEN/project/TemplateCxx/include/hello.h
---
+++ src/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "hello.h"
+
+void sayHello() {
+    std::cout << "hello world" << std::endl;
+}
